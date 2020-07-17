b305fd94c1f374918584be9a3e9df004c0ffc640
KeerthanaGirijan <67065992+KeerthanaGirijan@users.noreply.github.com>
2020-07-17 22:33:15 +0530

    Add files via upload

---
+++ Capstone project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is a notebook used for CAPSTONE project
+# 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as npl 
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
