3e30c95b568baf407c40a4f9de5c62e5bc6a6710
KeerthanaGirijan <67065992+KeerthanaGirijan@users.noreply.github.com>
2020-07-21 21:15:51 +0530

    Add files via upload

---
+++ 1st part.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  Project Description
+#  
+# One of an intriguing topic used in Data science is to compare neighborhoods of two cities or districts are similar or disimilar. People who are planning to do a bussiness, restaurants or want to construct a house would be benefitted from the comparison of these. The main purpose of the project is to ana;yse geographical locations and to help people understand the geographical advantages when starting or planning to do any sorts of investments.
+# 
+
+# Data used in this project
+# 
+# To make good use of the foursquare location data, the labs done for Toronto segmented and clustered neighborhoods gave me good examples for new ideas.
+# I would need to leverage the foursquare location data to solve or execute. Data science problems always target an audience and are meant to help a group of stakeholders solve a problem, so make sure that I explicitly describe the audience and why they would care about my problem.
+# From the results on New York, the lab especially tells 10 most common venues in each district. The data set is about Toronto Venues. After transforming the data to Json files, it is easy to use Pandas to transform them into DataFrame. Then select the required data columns to appear and get the information we want. Geopy library is used to get the latitude and longitude values. With the locations the maps can be created and we will have a direct understanding of the neighborhoods and the purposed areas. The Folium library is also used to show maps on different requirements
+# 
+
+# In[ ]:
+
+
+
+
